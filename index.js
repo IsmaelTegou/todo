f21c2fdbb26b46d3ee58c8205374598e6b55fa20
ismaelTegou <ismaeltegou7@gmail.com>
2025-07-07 08:01:06 +0200

    feature/#7-creation des middlewares supplementaires

--- index.js
+++ index.js
@@ -3,16 +3,29 @@ const sequelize = require('./config/database');
 const articleRoutes = require('./routes/articleRoutes');
 const chantRoutes = require('./routes/chant.routes');
 const categorieRoutes = require('./routes/categorie.routes');
-
 const app = express();
+
+//Middlewares generaux
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static('uploads'));
 
+//Routes API
 app.use("/api/articles", articleRoutes);
 app.use('/api/chants', chantRoutes);
 app.use('/api/categories', categorieRoutes);
-app.use('/uploads', express.static('uploads'));
 
+// Logger (optionnel)
+const logger = require('./middlewares/logger.middleware');
+app.use(logger);
+
+// Middleware 404
+const notFound = require('./middlewares/notfound.middleware');
+app.use(notFound);
+
+// Middleware erreurs
+const errorHandler = require('./middlewares/error.middleware');
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 sequelize.sync().then(() => {
